656ec4421308bc981aa3527539a50c77c8491d30
loduval <loduval@gmail.com>
2026-06-01 14:49:45 +0200

    add sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "jardin-v3";
+const ASSETS = [
+  "./", "./index.html", "./manifest.webmanifest",
+  "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./favicon-32.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => { try { c.put(e.request, copy); } catch (err) {} });
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
